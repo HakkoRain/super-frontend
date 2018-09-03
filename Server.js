9e5bdbfe1a80f0f7c113fe8e83481cf0a4204278
Bodya <grandrayhope@mail.ru>
2018-09-03 15:22:21 +0700

    comments

--- Server.js
+++ Server.js
@@ -6,16 +6,16 @@ const fs = require('fs')//Подключение модуля Fs для чете
 const server = http.createServer(function (request, Response) { //Встроенная команда CreateServer модуля Http, для создания сервера. Принимает функцию обработчика, которая принимает 2 аргумента.
     console.log(request.method, request.url); //На каждый запрос выводит сообщение в консоль; request.method - содержит метод запроса, request.url содержит адрес по которому выполнен запрос
     if (request.url == '/style.css') { 
-        const css = fs.readFileSync('style.css', 'utf8'); //Если происходит запрос к адресу Url, то отдаем на по запросу файл со стилями style.css
+        const css = fs.readFileSync('style.css', 'utf8'); //Если происходит запрос к адресу Url, то отдаем по запросу файл со стилями style.css
         Response.end(css); //Response имеет несколько методов, один из которых метод End, который сообщает о том, что запрос обработан и его можно завершить 
     } else { 
 
-    const html = fs.readFileSync('index.html', 'utf8');//Встроенная компанда readFileSync модуля Fs, для четения файла index.html в кодировке utf8
+    const html = fs.readFileSync('index.html', 'utf8');//Встроенная команда readFileSync модуля Fs, для четения файла index.html в кодировке utf8
     Response.end(html); //Отправляем содержимое файла index.html в ответ на запрос
     }
 });
 
-console.log('port = ', process.env.PORT); //Запуск сервера на специальном порту
+console.log('port = ', process.env.PORT); //Прием порта
 
 server.listen(process.env.PORT || 4000); //Команда для запуска сервера, в качестве аргумента указан порт для запуска сервера
 console.log('Server started!'); //Сообщение в консоль о том, что сервер запущен успешно.
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict'
 
-//script.js
 const fs = require('fs'); //подключили библиотеку (модуль) FS для работы с файловой системой
 
 const text = fs.readFileSync('script.js', 'utf8'); //прочитали файл script.js в кодировке utf8
-console.log(text);
+console.log(text); //Вывели прочитанное в консоль
